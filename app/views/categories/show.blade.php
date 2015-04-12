fb62111fda6548123816a7e0db3af074064cd2f5
Bunny <w@bunnywong.com>
2015-04-12 17:33:37 +0800

    [ Transaction / List view ] init

--- app/views/categories/show.blade.php
+++ app/views/categories/show.blade.php
@@ -1,7 +1,7 @@
 @extends('layouts.master')
 
 @section('title')
-文章分類頁 @parent
+Lunch Together @parent
 @stop
 
 @section('content')
@@ -9,33 +9,31 @@
 <div class="col-md-8">
 
     <h1 class="page-header">
-        偽 Blog
-        <small>文章分類頁</small>
+        Lunch Together
     </h1>
 
     @if (Auth::check())
     <div class="text-right">
-        <a class="btn btn-success" href="{{ route('posts.create') }}">新增文章</a>
+        <a class="btn btn-success" href="{{ route('posts.create') }}">New Payment</a>
     </div>
     @endif
-    
+
     @foreach($posts as $post)
     <h2>
         <a href="{{ route('posts.show', $post->id) }}">{{{ $post->title }}}</a>
     </h2>
     <p class="text-right">
-        <span>{{{ $post->category->name }}}</span>
-        <span class="glyphicon glyphicon-time"></span> 發表於 {{{ $post->created_at->toDateTimeString() }}}
+        <span class="glyphicon glyphicon-time"></span> {{{ $post->created_at->toDateTimeString() }}}
     </p>
-    <hr>
+    <p>${{{ $post->money }}} - {{{ $post->category->name }}}</p>
     <p>{{{ $post->content }}}</p>
-    
+
     <div class="text-right">
-        <a class="btn btn-info" href="{{ route('posts.show', $post->id) }}">閱讀全文</a>
+        <a class="btn btn-info" href="{{ route('posts.show', $post->id) }}">Read all</a>
         @if (Auth::check())
-        <a class="btn btn-primary" href="{{ route('posts.edit', $post->id) }}">編輯</a>
+        <a class="btn btn-primary" href="{{ route('posts.edit', $post->id) }}">Edit</a>
         {{ Form::open(['url' => 'posts/'.$post->id, 'method' => 'DELETE', 'style' => 'display: inline;', 'role' => 'form']) }}
-        {{ Form::submit('刪除', ['class' => 'btn btn-danger btn-sm']) }}
+        {{ Form::submit('Del', ['class' => 'btn btn-danger btn-sm']) }}
         {{ Form::close() }}
         @endif
     </div>
